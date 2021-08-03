5026a4108e97b62fd726b238dc3d9ac6018fdf89
Juan Cruz Martinez <ZOJCRUZM@zeiss.com>
2021-08-03 14:56:48 +0200

    design the UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>SEO Extension built with React!</h1>
+
+      <ul className="SEOForm">
+        <li className="SEOValidation">
+          <div className="SEOValidationField">
+            <span className="SEOValidationFieldTitle">Title</span>
+            <span className="SEOValidationFieldStatus Error">
+              90 Characters
+            </span>
+          </div>
+          <div className="SEOVAlidationFieldValue">
+            The title of the page
+          </div>
+        </li>
+
+        <li className="SEOValidation">
+          <div className="SEOValidationField">
+            <span className="SEOValidationFieldTitle">Number of Headings</span>
+            <span className="SEOValidationFieldStatus Ok">
+              Ok
+            </span>
+          </div>
+          <div className="SEOVAlidationFieldValue">
+            <ul>
+              <li>1 H1</li>
+              <li>3 H2</li>
+            </ul>
+          </div>
+        </li>
+      </ul>
     </div>
   );
 }
